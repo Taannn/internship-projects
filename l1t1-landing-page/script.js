d071fc6d487ea2e69357c72f691de8424db58037
Tristan Candelario <tristan.candelario06@gmail.com>
2024-09-08 16:59:45 +0800

    converted 2 navs to dynamic content
    Created a reusable function that for creating navbar links

--- l1t1-landing-page/script.js
+++ l1t1-landing-page/script.js
@@ -1,3 +1,4 @@
+// hamburger menu logic
 const menu = document.getElementById('menu');
 const close = document.querySelector('#menu i');
 const nav = document.querySelector('.mobile-nav');
@@ -15,3 +16,52 @@ menu.addEventListener("click", () => {
     nav.classList.remove('show');
   }
 });
+
+// DYNAMIC CONTENT
+
+// header nav
+const headerLinks = [
+  {
+    href: '#hero',
+    link: 'Home'
+  },
+  {
+    href: '#services',
+    link: 'Services'
+  },
+  {
+    href: '#features',
+    link: 'Features'
+  },
+  {
+    href: '#about',
+    link: 'About'
+  }
+];
+const headerNav = document.querySelector('.header-nav ul');
+const mobileNav = document.querySelector('.mobile-nav ul');
+
+const createNav = (links, element) => {
+
+  const createList = (link) => {
+    return  `
+      <li><a href="${link.href}">${link.link}</a></li>
+    `;
+  }
+
+  const navContent = `
+      ${links.map(link => createList(link)).join('')}
+  `;
+
+  element.innerHTML += navContent;
+};
+
+
+
+
+
+createNav(headerLinks, headerNav);
+createNav(headerLinks.concat([{
+  href: '#contact',
+  link: 'Contact Us'
+}]), mobileNav);
